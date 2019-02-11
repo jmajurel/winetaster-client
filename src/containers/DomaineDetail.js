7a0013a493379f64996cfff19b9f308045aea148
Jean-Felix Majurel <jeanfelix.majurel@gmail.com>
2019-02-11 17:20:35 +0000

    working on google map issue

--- src/containers/DomaineDetail.js
+++ src/containers/DomaineDetail.js
@@ -1,50 +1,51 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { fetchOneDomaine } from '../store/actions/domaines';
-import './DomaineDetail.css';
-import domaineDefault from '../images/domaine-default.jpg';
-import MapComponent from '../components/MapComponent';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPencilAlt } from '@fortawesome/free-solid-svg-icons'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchOneDomaine } from "../store/actions/domaines";
+import "./DomaineDetail.css";
+import domaineDefault from "../images/domaine-default.jpg";
+import MapComponent from "../components/MapComponent";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
 
 class DomaineDetail extends Component {
-
   componentWillMount() {
     this.props.fetchOneDomaine(this.props.match.params.id);
   }
 
   render() {
-    
     const { selectedDomaine } = this.props;
     const { name, description, picture, address, lat, lng } = selectedDomaine;
     return (
-      <div className='domaine-card'>
-        <div className='illustration'>
-	  <img src={picture || domaineDefault} alt={name}/>
-	</div>
+      <div className="domaine-card">
+        <div className="illustration">
+          <img src={picture || domaineDefault} alt={name} />
+        </div>
         <h2>Domaine {name}</h2>
-	{ address  && ( 
-	    <p>Address: <em>{address}</em></p>
-	)}
-	<p>{description}</p>
-	<Link 
-	  id='updateDomaine' 
-	  className='btn btn-warning edit' 
-	  to='/updateDomaine'>
-          <FontAwesomeIcon icon={faPencilAlt}/>
-	</Link>
-	{(lat && lng) && (
-	  <MapComponent 
-	    lat={lat}
-	    lng={lng}
-	    isMarkerShown
-	    googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_MAPKEY}&libraries=geometry,drawing,places`}
-	    loadingElement={<div style={{ height: `100%` }} />}
-	    containerElement={<div style={{ height: `400px` }} />}
-	    mapElement={<div style={{ height: `100%` }} />}
-	  /> 
-	)}
+        {address && (
+          <p>
+            Address: <em>{address}</em>
+          </p>
+        )}
+        <p>{description}</p>
+        <Link
+          id="updateDomaine"
+          className="btn btn-warning edit"
+          to="/updateDomaine"
+        >
+          <FontAwesomeIcon icon={faPencilAlt} />
+        </Link>
+        {lat && lng && (
+          <MapComponent
+            lat={lat}
+            lng={lng}
+            isMarkerShown
+            googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${REACT_APP_MAPKEY}&libraries=geometry,drawing,places`}
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `400px` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+          />
+        )}
       </div>
     );
   }
@@ -56,4 +57,7 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps, { fetchOneDomaine })(DomaineDetail);
+export default connect(
+  mapStateToProps,
+  { fetchOneDomaine }
+)(DomaineDetail);
